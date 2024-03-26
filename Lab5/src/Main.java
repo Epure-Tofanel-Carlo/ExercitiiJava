2c9693e0361896cec70de0cf96b074a2aa3a25b6
Epure-Tofanel-Carlo <129178776+Epure-Tofanel-Carlo@users.noreply.github.com>
2024-03-26 15:41:44 +0200

    lab5

---
+++ Lab5/src/Main.java
@@ -0,0 +1,10 @@
+import Regex.TryingRegex;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        TryingRegex obj = new TryingRegex();
+        obj.exercitiu();
+    }
+}
