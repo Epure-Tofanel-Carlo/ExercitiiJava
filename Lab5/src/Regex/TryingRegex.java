41392b1b45428f711657cc1095b335d1b1e9e9d8
Epure-Tofanel-Carlo <129178776+Epure-Tofanel-Carlo@users.noreply.github.com>
2024-03-26 15:02:57 +0200

    lab5

---
+++ Lab5/src/Regex/TryingRegex.java
@@ -0,0 +1,42 @@
+package Regex;
+
+public class TryingRegex {
+    public void exercitiu() {
+        String test = "a";
+        System.out.println(test.matches("[abc]"));
+        test = "e";
+        System.out.println(test.matches("[^abc]"));
+        test = "c";
+        System.out.println(test.matches("[a-z]"));
+        test = "A";
+        System.out.println(test.matches("[a-zA-Z]"));
+        test = "cD";
+        System.out.println(test.matches("[a-z][A-Z]"));
+        test = "abccaaaca";
+        System.out.println(test.matches("[abc]+"));
+        test = "abcabc";
+        System.out.println(test.matches("[abc]*"));
+        test = "abcac";
+        System.out.println(test.matches("[abc]{5}"));
+        test = "abacacc";
+        System.out.println(test.matches("[abc]{5,}"));
+        test = "ababababc";
+        System.out.println(test.matches("[abc]{5,10}"));
+
+        String text = new String("Odata creat un sir de caractere cu continutul sau nu mai poate fi modificat.");
+
+        System.out.println("Lungimea stringului: " + text.length());
+
+
+        System.out.println(text.replaceAll("[ .]","").matches("[a-zA-Z]*"));
+
+        String[] cuvinte = text.split(" ");
+        int middle = cuvinte.length / 2;
+        System.out.println(cuvinte[middle - 1] + " " + cuvinte[middle]);
+
+
+        StringBuilder sb = new StringBuilder(text);
+        System.out.println("Stringul inversat: " + sb.reverse());
+
+    }
+}
